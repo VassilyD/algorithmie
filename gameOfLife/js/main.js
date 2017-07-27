c637c5d90843928ded21c20ac0bdf36c119ae380
Vassily Dubois <vassily.dubois@gmail.com>
2017-07-27 02:58:07 +0200

    Game of life : leger amélioration perf

--- gameOfLife/js/main.js
+++ gameOfLife/js/main.js
@@ -7,6 +7,8 @@ function changerStatu(elem) {
 }
 
 function genTable(x = 10, y = 10) {
+	lignes = x;
+	colonnes = y;
 	for (var i = 0; i < x; i++) {
 		var ligne = document.createElement("tr");
 		document.body.appendChild(ligne);
@@ -28,25 +30,25 @@ function genTable(x = 10, y = 10) {
 
 function actuTable() {
 	var tmpt = [];
-	for (var i = 0; i < table.length; i++) {
+	for (var i = 0; i < lignes; i++) {
 		var fuck = [];
-		for (var j = 0; j < table[i].length; j++) {
+		for (var j = 0; j < colonnes; j++) {
 			fuck.push(table[i][j]);
 		}
 		tmpt.push(fuck);
 	}
-	for(var i = 0; i < table.length; i++) {
-		for (var j = 0; j < table[i].length; j++) {
+	for(var i = 0; i < lignes; i++) {
+		for (var j = 0; j < colonnes; j++) {
 			var voisin = 0;
 			for (var m = i-1; m <= i+1; m++) {
 				var y;
-				if(m == -1) y = table.length - 1;
-				else if(m == table.length) y = 0;
+				if(m == -1) y = lignes - 1;
+				else if(m == lignes) y = 0;
 				else y = m;
 				for (var n = j-1; n <= j+1; n++) {
 					var x;
-					if(n == -1) x = table[i].length - 1;
-					else if(n == table[i].length) x = 0;
+					if(n == -1) x = colonnes - 1;
+					else if(n == colonnes) x = 0;
 					else x = n;
 					if(!(m == i && n == j) && table[y][x]) voisin++;
 				}
@@ -57,14 +59,14 @@ function actuTable() {
 				}
 			}*/
 			tmpt[i][j] = (voisin == 3 || (table[i][j] && voisin == 2));
-			if(tmpt[i][j]) document.getElementById(i * table[0].length + j).className = 'alive';
-			else document.getElementById(i * table[0].length + j).className = 'dead';
+			if(tmpt[i][j]) document.getElementById(i * colonnes + j).className = 'alive';
+			else document.getElementById(i * colonnes + j).className = 'dead';
 		}
 	}
 	table = [];
-	for (var i = 0; i < tmpt.length; i++) {
+	for (var i = 0; i < lignes; i++) {
 		var fuck = [];
-		for (var j = 0; j < tmpt[i].length; j++) {
+		for (var j = 0; j < colonnes; j++) {
 			fuck.push(tmpt[i][j]);
 		}
 		table.push(fuck);
@@ -72,52 +74,9 @@ function actuTable() {
 }
 
 
-function reverseTable() {
-	var tmpt = [];
-	for (var i = 0; i < table.length; i++) {
-		var fuck = [];
-		for (var j = 0; j < table[i].length; j++) {
-			fuck.push(table[i][j]);
-		}
-		tmpt.push(fuck);
-	}
-	for(var i = 0; i < table.length; i++) {
-		for (var j = 0; j < table[i].length; j++) {
-			var voisin = 0;
-			for (var m = i-1; m <= i+1; m++) {
-				var y;
-				if(m == -1) y = table.length - 1;
-				else if(m == table.length) y = 0;
-				else y = m;
-				for (var n = j-1; n <= j+1; n++) {
-					var x;
-					if(n == -1) x = table[i].length - 1;
-					else if(n == table[i].length) x = 0;
-					else x = n;
-					if(!(m == i && n == j) && table[y][x]) voisin++;
-				}
-			}
-			/*for (var m = Math.max(0, i-1); m <= Math.min(i+1, table.length-1); m++) {
-				for (var n = Math.max(0, j-1); n <= Math.min(j+1, table[i].length-1); n++) {
-					if(!(m == i && n == j) && table[m][n]) voisin++;
-				}
-			}*/
-			tmpt[i][j] = (voisin == 3 || (table[i][j] && voisin == 2));
-			document.getElementById(i * table[0].length + j).className = (tmpt[i][j])?'alive':'dead';
-		}
-	}
-	table = [];
-	for (var i = 0; i < tmpt.length; i++) {
-		var fuck = [];
-		for (var j = 0; j < tmpt[i].length; j++) {
-			fuck.push(tmpt[i][j]);
-		}
-		table.push(fuck);
-	}
-}
-
-
-genTable(50,50);
+var lignes = 0;
+var colonnes = 0;
+genTable(150,150);
 var isAlive = 0;
 document.getElementById("launcher").onclick = function(){
 	if(isAlive != 0) {
@@ -127,33 +86,33 @@ document.getElementById("launcher").onclick = function(){
 	else isAlive = setInterval(actuTable, 100);
 }
 document.getElementById("nuke").onclick = function(){
-	for (var i = 0; i < table.length; i++) {
-		for (var j = 0; j < table[i].length; j++) {
+	for (var i = 0; i < lignes; i++) {
+		for (var j = 0; j < colonnes; j++) {
 			table[i][j] = false;
-			document.getElementById(i * table[0].length + j).className = 'dead';
+			document.getElementById(i * colonnes + j).className = 'dead';
 		}
 	}
 }
 document.getElementById("fill").onclick = function(){
-	for (var i = 0; i < table.length; i++) {
-		for (var j = 0; j < table[i].length; j++) {
+	for (var i = 0; i < lignes; i++) {
+		for (var j = 0; j < colonnes; j++) {
 			table[i][j] = true;
-			document.getElementById(i * table[0].length + j).className = 'alive';
+			document.getElementById(i * colonnes + j).className = 'alive';
 		}
 	}
 }
 document.getElementById("respawn").onclick = function(){
-	for (var i = 0; i < table.length; i++) {
-		for (var j = 0; j < table[i].length; j++) {
+	for (var i = 0; i < lignes; i++) {
+		for (var j = 0; j < colonnes; j++) {
 			table[i][j] = (Math.round(Math.random()) == 0);
-			document.getElementById(i * table[0].length + j).className = (table[i][j])?'alive':'dead';
+			document.getElementById(i * colonnes + j).className = (table[i][j])?'alive':'dead';
 		}
 	}
 }
 document.getElementById("plus").onclick = function(){
-	for (var i = 0; i < table.length; i++) {
-		for (var j = 0; j < table[i].length; j++) {
-			var elem = document.getElementById(i * table[0].length + j);
+	for (var i = 0; i < lignes; i++) {
+		for (var j = 0; j < colonnes; j++) {
+			var elem = document.getElementById(i * colonnes + j);
 			var tmp = elem.offsetWidth + 1
 			elem.style.width = tmp + 'px';
 			var tmp = elem.offsetHeight + 1
@@ -162,9 +121,9 @@ document.getElementById("plus").onclick = function(){
 	}
 }
 document.getElementById("moins").onclick = function(){
-	for (var i = 0; i < table.length; i++) {
-		for (var j = 0; j < table[i].length; j++) {
-			var elem = document.getElementById(i * table[0].length + j);
+	for (var i = 0; i < lignes; i++) {
+		for (var j = 0; j < colonnes; j++) {
+			var elem = document.getElementById(i * colonnes + j);
 			elem.style.width = (elem.offsetWidth - 1) + 'px';
 			elem.style.height = (elem.offsetHeight - 1) + 'px';
 		}
