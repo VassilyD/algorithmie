f9dbc0b6bc90d9fb60dcacafdd801df53285e99d
Vassily Dubois <vassily.dubois@gmail.com>
2017-07-27 02:48:27 +0200

    Game of life : ajout option remplir, vider, respawn, zoommer/dezoomer (nonfonvtionnel)

--- gameOfLife/js/main.js
+++ gameOfLife/js/main.js
@@ -37,7 +37,6 @@ function actuTable() {
 	}
 	for(var i = 0; i < table.length; i++) {
 		for (var j = 0; j < table[i].length; j++) {
-			//document.getElementById(i * table[0].length + j).style.opacity = 0;
 			var voisin = 0;
 			for (var m = i-1; m <= i+1; m++) {
 				var y;
@@ -60,8 +59,6 @@ function actuTable() {
 			tmpt[i][j] = (voisin == 3 || (table[i][j] && voisin == 2));
 			if(tmpt[i][j]) document.getElementById(i * table[0].length + j).className = 'alive';
 			else document.getElementById(i * table[0].length + j).className = 'dead';
-			//document.getElementById(i * table[0].length + j).style.opacity = 1;
-			// 
 		}
 	}
 	table = [];
@@ -73,6 +70,53 @@ function actuTable() {
 		table.push(fuck);
 	}
 }
+
+
+function reverseTable() {
+	var tmpt = [];
+	for (var i = 0; i < table.length; i++) {
+		var fuck = [];
+		for (var j = 0; j < table[i].length; j++) {
+			fuck.push(table[i][j]);
+		}
+		tmpt.push(fuck);
+	}
+	for(var i = 0; i < table.length; i++) {
+		for (var j = 0; j < table[i].length; j++) {
+			var voisin = 0;
+			for (var m = i-1; m <= i+1; m++) {
+				var y;
+				if(m == -1) y = table.length - 1;
+				else if(m == table.length) y = 0;
+				else y = m;
+				for (var n = j-1; n <= j+1; n++) {
+					var x;
+					if(n == -1) x = table[i].length - 1;
+					else if(n == table[i].length) x = 0;
+					else x = n;
+					if(!(m == i && n == j) && table[y][x]) voisin++;
+				}
+			}
+			/*for (var m = Math.max(0, i-1); m <= Math.min(i+1, table.length-1); m++) {
+				for (var n = Math.max(0, j-1); n <= Math.min(j+1, table[i].length-1); n++) {
+					if(!(m == i && n == j) && table[m][n]) voisin++;
+				}
+			}*/
+			tmpt[i][j] = (voisin == 3 || (table[i][j] && voisin == 2));
+			document.getElementById(i * table[0].length + j).className = (tmpt[i][j])?'alive':'dead';
+		}
+	}
+	table = [];
+	for (var i = 0; i < tmpt.length; i++) {
+		var fuck = [];
+		for (var j = 0; j < tmpt[i].length; j++) {
+			fuck.push(tmpt[i][j]);
+		}
+		table.push(fuck);
+	}
+}
+
+
 genTable(50,50);
 var isAlive = 0;
 document.getElementById("launcher").onclick = function(){
@@ -82,3 +126,48 @@ document.getElementById("launcher").onclick = function(){
 	}
 	else isAlive = setInterval(actuTable, 100);
 }
+document.getElementById("nuke").onclick = function(){
+	for (var i = 0; i < table.length; i++) {
+		for (var j = 0; j < table[i].length; j++) {
+			table[i][j] = false;
+			document.getElementById(i * table[0].length + j).className = 'dead';
+		}
+	}
+}
+document.getElementById("fill").onclick = function(){
+	for (var i = 0; i < table.length; i++) {
+		for (var j = 0; j < table[i].length; j++) {
+			table[i][j] = true;
+			document.getElementById(i * table[0].length + j).className = 'alive';
+		}
+	}
+}
+document.getElementById("respawn").onclick = function(){
+	for (var i = 0; i < table.length; i++) {
+		for (var j = 0; j < table[i].length; j++) {
+			table[i][j] = (Math.round(Math.random()) == 0);
+			document.getElementById(i * table[0].length + j).className = (table[i][j])?'alive':'dead';
+		}
+	}
+}
+document.getElementById("plus").onclick = function(){
+	for (var i = 0; i < table.length; i++) {
+		for (var j = 0; j < table[i].length; j++) {
+			var elem = document.getElementById(i * table[0].length + j);
+			var tmp = elem.offsetWidth + 1
+			elem.style.width = tmp + 'px';
+			var tmp = elem.offsetHeight + 1
+			elem.style.height = tmp + 'px';
+		}
+	}
+}
+document.getElementById("moins").onclick = function(){
+	for (var i = 0; i < table.length; i++) {
+		for (var j = 0; j < table[i].length; j++) {
+			var elem = document.getElementById(i * table[0].length + j);
+			elem.style.width = (elem.offsetWidth - 1) + 'px';
+			elem.style.height = (elem.offsetHeight - 1) + 'px';
+		}
+	}
+}
+document.getElementById("onePass").onclick = actuTable;
